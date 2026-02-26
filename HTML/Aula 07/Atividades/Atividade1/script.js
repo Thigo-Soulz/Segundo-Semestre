632205beecd64db605baf881ad375d14ea7dcc61
Thigo-Soulz <thiago.vilhena46@gmail.com>
2026-02-26 11:30:18 -0300

    Auto commit - alteracao detectada

--- HTML/Aula 07/Atividades/Atividade1/script.js
+++ HTML/Aula 07/Atividades/Atividade1/script.js
@@ -47,12 +47,23 @@ console.log(data_analyst_ativos)
 console.log(dev_4000)
 console.log(manager_30)
 
-document.addEventListener("DOMContentLoaded", function () {
+let container = document.getElementById('container')
+
+function createCard(users){
+    let div = document.createElement('div')
+    let span = document.createElement('span')
+    span.innerHTML = `${users.name} ${users.age}`
+    div.appendChild(span)
+
+    return div
+}
+
+document.addEventListener("DOMContentLoaded", function(){
 
     let container = document.getElementById('container')
 
-    func_Ativos.forEach(user => {
-        const card = createCard(user)
+    func_Ativos.forEach(pessoa => {
+        const card = createCard(pessoa)
         container.appendChild(card)
     })
 
