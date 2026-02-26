7db7ec57aa6f76fe29beb9265c7c7c4d9e920227
Thigo-Soulz <thiago.vilhena46@gmail.com>
2026-02-26 09:05:10 -0300

    Auto commit - alteracao detectada

---
+++ HTML/Aula 07/script.js
@@ -0,0 +1,23 @@
+const pessoas = []
+
+const pessoa = {
+    name : "Erich",
+    lastname: "Natal",
+    age : 22 
+}
+const pessoa2 = {
+    name : "Igor",
+    lastname: "Machado",
+    age : 20
+}
+const pessoa3 = {
+    name : "Maria",
+    lastname: "Clara",
+    age : 18 
+}
+
+pessoas.push(pessoa)
+pessoas.push(pessoa2)
+pessoas.push(pessoa3)
+
+console.log(pessoas)
