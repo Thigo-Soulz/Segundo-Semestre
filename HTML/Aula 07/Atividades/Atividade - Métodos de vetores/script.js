fc370e2ca63d1b6768045e89b0418249830d248f
Thigo-Soulz <thiago.vilhena46@gmail.com>
2026-02-26 10:44:47 -0300

    Auto commit - alteracao detectada

---
+++ HTML/Aula 07/Atividades/Atividade - Métodos de vetores/script.js
@@ -0,0 +1,23 @@
+const users = [
+    { id: 1, name: 'Ana', age: 22, salary: 3500, active: true, role: 'dev' },
+    { id: 2, name: 'Carlos', age: 29, salary: 7200, active: false, role: 'manager' },
+    { id: 3, name: 'Marina', age: 31, salary: 6800, active: true, role: 'designer' },
+    { id: 4, name: 'João', age: 19, salary: 2500, active: true, role: 'dev' },
+    { id: 5, name: 'Fernanda', age: 27, salary: 4100, active: false, role: 'designer' },
+    { id: 6, name: 'Lucas', age: 35, salary: 9500, active: true, role: 'manager' },
+    { id: 7, name: 'Beatriz', age: 24, salary: 3900, active: true, role: 'dev' },
+    { id: 8, name: 'Rafael', age: 33, salary: 7800, active: true, role: 'data_analyst' },
+    { id: 9, name: 'Juliana', age: 26, salary: 5200, active: true, role: 'data_analyst' },
+    { id: 10, name: 'Bruno', age: 41, salary: 11000, active: false, role: 'manager' },
+    { id: 11, name: 'Camila', age: 30, salary: 6300, active: true, role: 'designer' },
+    { id: 12, name: 'Thiago', age: 28, salary: 4700, active: true, role: 'dev' },
+    { id: 13, name: 'Patricia', age: 37, salary: 8800, active: true, role: 'data_analyst' },
+    { id: 14, name: 'Gustavo', age: 23, salary: 3100, active: false, role: 'dev' },
+    { id: 15, name: 'Larissa', age: 34, salary: 7600, active: true, role: 'manager' }
+    ]
+
+
+const func_Ativos = pessoas.filter((p) => p.active == "true")
+const func_inativos = pessoas.filter((p) => p.active == "false")
+const devs = pessoas.filter((p) => p.role == "dev")
+
